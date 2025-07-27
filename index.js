f7d776aca87a9efcc0093fd221ce12df1a147d20
dal gobbo theo <theo.dal_gobbo@edu.univ-fcomte.fr>
2025-07-27 18:10:41 +0200

    connection port

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const bodyParser = require('body-parser');
 const discsRoutes = require('./routes/discsRoutes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/', discsRoutes);
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`URLCustomDiscs API listening on port ${port}`);
 });
